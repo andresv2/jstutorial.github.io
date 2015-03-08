7e665471b32827b5d97c3a85f9fe6e038480482f
Andres Valencia <andresv2@mac.com>
2015-03-08 14:35:29 -0400

    initial commit

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  // Provide your access token
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYXZyZXMwMyIsImEiOiJzNWE3SDdZIn0.YQR58MWexEblFOztOGK0ig';
+  // Create a map in the div #map
+  L.mapbox.map('map', 'avres03.lcm6deo9').setView([40.771443,-73.982980], 16);
+
+   L.mapbox.map('ues-map', 'avres03.lcm6deo9').setView([40.773321,-73.955530], 16);
+
+
+})
+
+
+
+
+// var zoom = 16;
+// var center = [40.779165,-73.962928];
+// var map = L.map('map').setView(center, zoom);
+
+
+
+// Upper East Side
+
+// Chelsea
+
+// West Village
+
+// East Village
+
+// Lower East Side
+
+// Flatiron
+
+// Brooklyn
+
+
+
+
+
+
+
+
+// var zoom = 12;
+// var center = [40.658528, -73.952551]; //set center coordinates
+// var map = L.map('map').setView(center, zoom);
---
+++ app/models/coffee.rb
@@ -0,0 +1,22 @@
+require 'httparty'
+
+class Coffee
+  attr_accessor :client
+
+  CLIENT_ID=ENV['FOUR_SQUARE_CLIENT']
+  CLIENT_SECRET=ENV['FOUR_SQUARE_SECRET']
+
+  def initialize
+    @client = Foursquare2::Client.new({
+      :client_id => ENV['FOUR_SQUARE_CLIENT'],
+      :client_secret => ENV['FOUR_SQUARE_SECRET']
+    })
+  end
+
+  def self.search
+    date = '0150305' # YYYYMMDD
+    url="https://api.foursquare.com/v2/venues/search?radius=800&client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{date}"
+    HTTParty.get url
+  end
+
+end
